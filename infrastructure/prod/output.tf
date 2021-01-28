36fac7c4151ea4213eecaefdd1aa4d6943141456
Michael Greenly <mgreenly@logic-refinery.com>
2021-01-27 19:50:33 -0600

    cleanup prod output

--- infrastructure/prod/output.tf
+++ infrastructure/prod/output.tf
@@ -8,23 +8,5 @@ output "war" {
       public_ip: aws_instance.server.public_ip,
       private_ip: aws_instance.server.private_ip
     }
-    # git: {
-    #   clone_url_ssh: aws_codecommit_repository.war.clone_url_ssh,
-    #   clone_url_http: aws_codecommit_repository.war.clone_url_http,
-    # },
-     # ecr: {
-     #   baseimage: {
-     #     repository_id: aws_ecr_repository.baseimage.registry_id,
-     #     repository_url: aws_ecr_repository.baseimage.repository_url
-     #   },
-     #   builder_ghc: {
-     #     repository_id: aws_ecr_repository.builder_ghc.registry_id,
-     #     repository_url: aws_ecr_repository.builder_ghc.repository_url
-     #   },
-     #   backend: {
-     #     repository_id: aws_ecr_repository.backend.registry_id,
-     #     repository_url: aws_ecr_repository.backend.repository_url
-     #   }
-    # }
   }
 }
